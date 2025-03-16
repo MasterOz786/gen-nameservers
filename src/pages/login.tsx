e601260459763dff504fbbbfa7f60dcc446df39b
Anoosha Ali <155889714+AnooshaaAli@users.noreply.github.com>
2025-03-16 22:17:30 +0500

    removed authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,6 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
 import { useToast } from '../components/ui/use-toast'
-import { authApi } from '../lib/api'
 import { useAuthStore } from '../lib/store'
 
 export function Login() {
